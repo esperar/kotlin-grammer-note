1207c40e6585586ca2dd932433aa031ff7059e3e
KimHuiMang <piyrw9754@gmail.com>
2022-12-13 12:13:01 +0900

    variable

--- src/main/kotlin/variable/Main.kt
+++ src/main/kotlin/variable/Main.kt
@@ -1,2 +1,22 @@
 package variable
 
+fun main() {
+    var a:Int = 1
+    val b:Int = 3
+
+    println(a);
+    println(b);
+
+    var charVariable:Char = 'a'
+    val charVariable2 = 'a' // nullable
+
+    var stringVariable = "Hi I'm hope"
+    val stringVariable2 = """
+        Hello My Name is Kim Hope. 
+        Nice to meet you.
+    """
+
+    // var 기본적인 변수 val 초기화만 가능 변경 불가능
+    // 변수명:type 타입 지정 -> null을 허용하지 않음
+    // type 지정을 하지않으면 nullable 신중히 사용해야함
+}
