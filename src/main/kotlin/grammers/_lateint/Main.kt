c20b128479b8526f38de5fd58e120667376bc0a9
aesperer <piyrw9754@gmail.com>
2023-01-25 23:02:56 +0900

    lateInit

---
+++ src/main/kotlin/grammers/_lateint/Main.kt
@@ -0,0 +1,22 @@
+package grammers._lateint
+
+fun main() {
+
+    val a = LateInitSample()
+
+    println(a.getLateInitText())
+    a.text = "새로운 할당 값"
+    println(a.getLateInitText())
+}
+
+class LateInitSample {
+    lateinit var text: String
+
+    fun getLateInitText(): String {
+        if(::text.isInitialized) {
+            return text
+        } else {
+            return "기본값"
+        }
+    }
+}
