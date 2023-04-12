6b52b9872f3ea5c2944a6d9ffbc95e30f38e2f6d
aesperer <piyrw9754@gmail.com>
2023-04-12 15:18:35 +0900

    delegator observable 사용해 위임

--- src/main/kotlin/in_action/supports/Support.kt
+++ src/main/kotlin/in_action/supports/Support.kt
@@ -1,6 +1,7 @@
 package in_action.supports
 
 import java.beans.PropertyChangeSupport
+import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
 class ObservableProperty(
@@ -14,13 +15,14 @@ class ObservableProperty(
         changeSupport.firePropertyChange(prop.name, oldValue, newValue)
     }
 
-
-
 }
 
 class Person(
     val name: String, age: Int, salary: Int
-) : PropertyChangeAware(){
-    var age: Int by ObservableProperty(age, changeSupport)
-    var salary: Int by ObservableProperty(salary, changeSupport)
+) : PropertyChangeAware() {
+    private val observer = { prop: KProperty<*>, oldValue: Int, newValue: Int ->
+        changeSupport.firePropertyChange(prop.name, oldValue, newValue)
+    }
+    var age: Int by Delegates.observable(age, observer)
+    var salary: Int by Delegates.observable(salary, observer)
 }
